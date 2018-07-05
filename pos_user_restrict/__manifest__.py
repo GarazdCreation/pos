a64e2760f718e5bce7b2b3ff6117fb95f03e58a7
Razumovskyi Yurii <GarazdCreation@gmail.com>
2018-07-05 23:22:49 +0300

    [ADD] pos_user_restrict: add banner.png

--- pos_user_restrict/__manifest__.py
+++ pos_user_restrict/__manifest__.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
+#############################################################
+#
+#    Garazd Creation, Ukraine
+#    Copyright (C) 2018 Garazd Creation (<https://garazd.biz/>).
+#    Author: Yurii Razumovskyi (<support@garazd.biz>)
+#
+#############################################################
 {
-    'name': 'Garazd restriction of POS User',
-    'version': '10.0.1.0.1',
-    'category': 'Point of Sales',
+    'name': 'Restriction of POS User',
+    'version': '10.0.1.0.2',
+    'category': 'Point of Sale',
     'author': 'Garazd Creation',
-    'website': "https://garazd.biz",
-    'summary': """
-        Limit the POS User to available Points of Sale.
-    """,
+    'website': 'https://garazd.biz',
+    'license': 'LGPL-3',
+    'summary': """Limits the POS User to available Points of Sale.""",
     'description': 'Allow setting available Points of Sales for users. Restrict access for POS users to Points of Sales.',
+    'images': ['static/description/banner.png'],
     'depends': [
         'point_of_sale',
     ],
