f576a571b69510b8b680fc04d91495f2e21c613e
Razumovskyi Yurii <GarazdCreation@gmail.com>
2018-07-06 00:47:37 +0300

    [IMP] pos_user_restrict: change banner.png

--- pos_user_restrict/__manifest__.py
+++ pos_user_restrict/__manifest__.py
@@ -14,7 +14,7 @@
     'website': 'https://garazd.biz',
     'license': 'LGPL-3',
     'summary': """Limits the POS User to available Points of Sale.""",
-    'description': 'Allow setting available Points of Sales for users. Restrict access for POS users to Points of Sales.',
+    'description': 'Allow setting available Points of Sales for users. Restricts access for POS users to Points of Sales.',
     'images': ['static/description/banner.png'],
     'depends': [
         'point_of_sale',
