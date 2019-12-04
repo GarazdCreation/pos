ea4ed86d0beed93b2c4d4bfed0304420c6f6d838
Razumovskyi Yurii <GarazdCreation@gmail.com>
2019-12-04 15:44:48 +0200

    [FIX] pos_user_restrict: add cache_clearing on changing allowed POS

--- pos_user_restrict/models/res_users.py
+++ pos_user_restrict/models/res_users.py
@@ -2,7 +2,20 @@
 
 from odoo import fields, models
 
+
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    pos_config_ids = fields.Many2many('pos.config', string='Available POS', help="Available POS for users. POS managers can view all POS configs.")
+    pos_config_ids = fields.Many2many(
+        'pos.config',
+        string='Allowed POS',
+        help="Allowed POS for users. POS managers can view all Points of Sale.",
+    )
+
+    def write(self, values):
+        res = super(ResUsers, self).write(values)
+        if 'pos_config_ids' in values:
+            self.env['ir.model.access'].call_cache_clearing_methods()
+            self.env['ir.rule'].clear_caches()
+            self.has_group.clear_cache(self)
+        return res
